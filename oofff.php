d780e2961a55833e717bc46c19de835bf7769384
Brian Samonte <45224089+Briaaaan@users.noreply.github.com>
2019-02-20 14:49:45 +0800

    Add files via upload

---
+++ oofff.php
@@ -0,0 +1,14 @@
+<?php
+
+ echo "First Name: "; echo $_POST["fname"];
+ echo "<br>";
+ echo "Last Name: "; echo $_POST["lname"]	;
+ echo "<br>";
+ echo "Birth Date: "; echo $_POST["birthday"];
+ echo "<br>";
+ echo "Gender: "; echo $_POST["subject"];
+ echo "<br>";
+ echo "Email: "; echo $_POST["email"];
+ echo "<br>";
+ echo "Phone: "; echo $_POST["phone"];
+?>
